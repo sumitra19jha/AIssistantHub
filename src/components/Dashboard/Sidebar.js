e3232647674d85ac615af38e7eb28aa791122b07
Sumitra Saksham <sumitra19jha@gmail.com>
2023-04-24 13:39:43 +0000

    Infinite scrolling for projects

--- src/components/Dashboard/Sidebar.js
+++ src/components/Dashboard/Sidebar.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FaSignOutAlt, FaHome, FaShoppingCart, FaCog, FaInfoCircle } from 'react-icons/fa';
 import "./Sidebar.css";
 
 const Sidebar = () => {
+    const location = useLocation();
+
+    const isActive = (path) => {
+        return location.pathname === path ? "active-link" : "";
+    }
+
     return (
         <div className="sidebar">
             <div className="sidebar-logo">
@@ -11,25 +17,25 @@ const Sidebar = () => {
             </div>
             <ul>
                 <li>
-                    <Link to="/dashboard">
+                    <Link to="/dashboard" className={`sidebar-link ${isActive("/dashboard")}`}>
                         <FaHome className="menu-icon" />
                         My Dashboard
                     </Link>
                 </li>
                 <li>
-                    <Link to="/user-subscription">
+                    <Link to="/user-subscription" className={`sidebar-link ${isActive("/user-subscription")}`}>
                         <FaShoppingCart className="menu-icon" />
                         My Purchase
                     </Link>
                 </li>
                 <li>
-                    <Link to="/settings">
+                    <Link to="/settings" className={`sidebar-link ${isActive("/settings")}`}>
                         <FaCog className="menu-icon" />
                         My Settings
                     </Link>
                 </li>
                 <li>
-                    <Link to="/help">
+                    <Link to="/help" className={`sidebar-link ${isActive("/help")}`}>
                         <FaInfoCircle className="menu-icon" />
                         Help Center
                     </Link>
--- src/components/ProjectHistory/ProjectHistory.js
+++ src/components/ProjectHistory/ProjectHistory.js
@@ -1,46 +1,69 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext, useRef } from 'react';
 import ProjectCard from '../ProjectCard/ProjectCard';
 import { ProjectContext } from '../../context/ProjectContext';
 import api from '../../services/api';
 import './ProjectHistory.css';
 
 const ProjectHistory = () => {
+    const [firstloading, setFirstLoading] = useState(true);
     const [page, setPage] = useState(1);
-    const [perPage, setPerPage] = useState(12);
+    const perPage = 12;
     const { projects, setProjects, totalPages, setTotalPages } = useContext(ProjectContext);
+    const lastProjectCardRef = useRef();
 
     useEffect(() => {
         fetchProjects();
     }, [page]);
 
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                if (entries[0].isIntersecting && page < totalPages) {
+                    setPage((prevPage) => prevPage + 1);
+                }
+            },
+            { threshold: 1 }
+        );
+
+        if (lastProjectCardRef.current) {
+            observer.observe(lastProjectCardRef.current);
+        }
+        
+        return () => {
+            if (lastProjectCardRef.current) {
+                observer.unobserve(lastProjectCardRef.current);
+            }
+        };
+    }, [projects, totalPages]);
+
     const fetchProjects = async () => {
+        if (page > totalPages && !firstloading) { return; }
         api.get(`/dashboard/history/content?page=${page}&per_page=${perPage}`)
             .then((response) => {
                 if (response.data.success) {
-                    setProjects(response.data.history);
+                    setProjects((prevProjects) => [...prevProjects, ...response.data.history]);
                     setTotalPages(response.data.pagination.total_pages);
+                    setFirstLoading(false);
                 } else {
                     alert(response.data.message);
+                    setFirstLoading(false);
                 }
             })
             .catch((error) => {
                 alert("Some issue occurred!");
+                setFirstLoading(false);
             });
-    };
-
-    const handlePreviousPage = () => {
-        if (page > 1) setPage(page - 1);
-    };
-
-    const handleNextPage = () => {
-        if (page < totalPages) setPage(page + 1);
     };
 
     return (
         <div className="project-history">
             {projects.length > 0 ? (
                 projects.map((project, index) => (
-                    <ProjectCard key={index} project={project} />
+                    <ProjectCard
+                        key={index}
+                        project={project}
+                        ref={index === projects.length - 1 ? lastProjectCardRef : null}
+                    />
                 ))
             ) : (
                 <div className="no-project-history">
@@ -51,25 +74,6 @@ const ProjectHistory = () => {
                     <p>There are no projects in your history. Start by creating a new project.</p>
                 </div>
             )}
-            <div className="pagination">
-                <button
-                    className="pagination-button"
-                    onClick={handlePreviousPage}
-                    disabled={page === 1}
-                >
-                    Previous
-                </button>
-                <span className="pagination-info">
-                    Page {page} of {totalPages}
-                </span>
-                <button
-                    className="pagination-button"
-                    onClick={handleNextPage}
-                    disabled={page === totalPages}
-                >
-                    Next
-                </button>
-            </div>
         </div>
     );
 };
